0835fbfc12e83b04a4eb8ffcf36160c5c701dfb2
Lucas Henrique Procopio <procmode.dev@gmail.com>
2019-05-01 15:07:18 -0300

    feat: Update AddEntry component
    Add react native buttons and sliders to controll fitness data

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
 import { getMetricMetaInfo, timeToString } from '../utils/helpers'
-import Slider from './Slider'
+import SliderFit from './SliderFit'
 import Stepper from './Stepper'
 import DateHeader from './DateHeader'
+import { Ionicons } from '@expo/vector-icons'
+import TextButton from './TextButton'
 
 function SubmitBtn({ onPress }) {
   return (
@@ -29,7 +31,7 @@ export default class AddEntry extends React.Component {
       const count = currState[metric] + step
 
       return {
-        ...state,
+        ...currState,
         [metric]: count > max ? max : count
       }
     })
@@ -40,7 +42,7 @@ export default class AddEntry extends React.Component {
       const count = currState[metric] - getMetricMetaInfo(metric).step
 
       return {
-        ...state,
+        ...currState,
         [metric]: count < 0 ? 0 : count
       }
     })
@@ -70,12 +72,26 @@ export default class AddEntry extends React.Component {
     //todo: clear local notifications
   }
 
+  reset = () => {
+    const key = timeToString()
+
+    //todo: update redux , route to home , DB
+  }
   render() {
     const metaInfo = getMetricMetaInfo()
+
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons name="ios-happy" size={100} />
+          <Text>You already logged your information for today</Text>
+          <TextButton onPress={this.reset}>Reset</TextButton>
+        </View>
+      )
+    }
     return (
       <View>
         <DateHeader date={new Date().toLocaleDateString()} />
-        <Text>{JSON.stringify(this.state)}</Text>
         {Object.keys(metaInfo).map(key => {
           const { getIcon, type, ...rest } = metaInfo[key]
           const value = this.state[key]
@@ -84,7 +100,7 @@ export default class AddEntry extends React.Component {
             <View key={key}>
               {getIcon()}
               {type === 'slider' ? (
-                <Slider
+                <SliderFit
                   value={value}
                   onChange={value => this.slide(key, value)}
                   {...rest}
